17fd1f075371162bcf77e5e8b1e98d310e750883
aswmac <aswmac@yahoo.com>
2026-07-11 15:27:05 -0500

    Refactor ContentView parameters in MoonStrokeApp

--- MoonStrokeApp.swift
+++ MoonStrokeApp.swift
@@ -2,9 +2,13 @@ import SwiftUI
 
 @main
 struct MoonStrokeDocumentApp: App {
-    var body: some Scene {
-        DocumentGroup(newDocument: MoonStrokeDocument()) { file in
-					ContentView(document: file.$document.content, nib: file.$document.nib)
-        }
+
+  let appHyperMode = HyperMode(type: .pen) // there is only one app and one mode at a time
+  let appNib = NibMatrix() // there is only one pencil and one nib at a time
+  
+  var body: some Scene {
+    DocumentGroup(newDocument: MoonStrokeDocument()) { file in
+      ContentView(document_0000: file.$document.content, appNib: appNib, appHyperMode: appHyperMode)
     }
+  }
 }
